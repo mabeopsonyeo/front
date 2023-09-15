496c93b61a462f7502cc1f5106c9f5512860906f
kroongft <krooooong@naver.com>
2023-09-15 16:20:11 +0900

    add: DefaultLayout 페이지별 background image 변경

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,19 +1,44 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useMemo } from 'react';
+import { useMatch } from 'react-router-dom';
 import styled from 'styled-components';
+import mainBackgroundImage from '@/assets/images/mainBackgroundImage.jpg';
+import evenQuestionsBackgroundImage from '@/assets/images/evenQuestionsBackgroundImage.jpg';
+import oddQuestionsBackgroundImage from '@/assets/images/oddQuestionsBackgroundImage.jpg';
 
 export const DefaultLayout = (props: { children: ReactNode | undefined }) => {
+  const matchMain = useMatch('/');
+  const matchQuestions = useMatch('/questions');
+  const matchResults = useMatch('/results');
+
+  const backgroundImageURL = useMemo(() => {
+    let imageURL = '' as string;
+    if (matchMain) {
+      imageURL = mainBackgroundImage;
+    }
+    if (matchQuestions) {
+      imageURL = evenQuestionsBackgroundImage;
+    }
+    if (matchResults) {
+      imageURL = oddQuestionsBackgroundImage;
+    }
+    return imageURL;
+  }, [matchMain, matchQuestions, matchResults]);
+
   return (
-    <LayoutContainer>
+    <LayoutContainer backgroundImageURL={backgroundImageURL}>
       <LayoutWrapper>{props.children}</LayoutWrapper>
     </LayoutContainer>
   );
 };
 
-const LayoutContainer = styled.div`
-  width: 100%;
+const LayoutContainer = styled.div<{ backgroundImageURL: string }>`
   display: flex;
   justify-content: center;
   height: 100vh;
+  width: 100vw;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(${({ backgroundImageURL }) => backgroundImageURL});
 `;
 
 const LayoutWrapper = styled.div`
