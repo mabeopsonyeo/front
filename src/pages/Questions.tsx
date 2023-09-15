9cf0ee22457dfbacb092ab6a24ef896df0cb18ef
Diane <yejimun.dev@gmail.com>
2023-09-15 17:14:26 +0900

    fix: eslint warning 제거

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import { useRecoilState } from 'recoil';
-import { answerState, stepState } from '@/recoil/state';
-
-import { test } from '@/constant/questions';
 
 export const Questions = () => {
-  const [answers, setAnswers] = useRecoilState(answerState);
-  const [step, setStep] = useRecoilState(stepState);
-
-  return <div>{test[step].question}</div>;
+  return <div>Qusetions</div>;
 };
