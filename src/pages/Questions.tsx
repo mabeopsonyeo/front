6c9d9ce64317bc499a5db6b2397f60f5110ca3df
Diane <yejimun.dev@gmail.com>
2023-09-15 16:58:57 +0900

    add: recoil 설정

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { answerState, stepState } from '@/recoil/state';
+
+import { test } from '@/constant/questions';
 
 export const Questions = () => {
-  return <div>Questios</div>;
+  const [answers, setAnswers] = useRecoilState(answerState);
+  const [step, setStep] = useRecoilState(stepState);
+
+  return <div>{test[step].question}</div>;
 };
