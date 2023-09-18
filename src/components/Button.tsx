b0c2733ad10e57a9aeef381b194eff59e63ffc75
kroongft <krooooong@naver.com>
2023-09-18 15:17:15 +0900

    add: Button 컴포넌트 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,46 @@
+import theme from '@/styles/theme';
+import React, { ReactElement, ReactNode, useMemo } from 'react';
+import { styled } from 'styled-components';
+
+interface ButtonProps {
+  type?: 'hoshinoPurple' | 'aiYellow' | 'rubyPink' | 'aquaBlue';
+  children: ReactNode | undefined;
+  onClick?: any;
+}
+
+export const Button = (props: ButtonProps) => {
+  const { type = 'hoshinoPurple', children, onClick } = props;
+  const backgroundColor = `${theme.color[type].default}`;
+  const hoverColor = useMemo(() => {
+    if (type === 'aiYellow') {
+      return `${theme.color[type].scale300}`;
+    }
+    if (type === 'rubyPink' || type === 'aquaBlue') {
+      return `${theme.color[type].scale500}`;
+    }
+    return `${theme.color[type].scale900}`;
+  }, [type]);
+
+  return (
+    <ButtonWrapper backgroundColor={backgroundColor} hoverColor={hoverColor} onClick={onClick}>
+      {children}
+    </ButtonWrapper>
+  );
+};
+
+const ButtonWrapper = styled.button<{ backgroundColor: string; hoverColor: string }>`
+  width: 100%;
+  height: 74px;
+  border-radius: 16px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: #fff;
+  text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.4);
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+
+  &:hover {
+    background-color: ${({ hoverColor }) => hoverColor};
+  }
+`;
