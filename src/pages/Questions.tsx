850bef8382fe06fd6753e03939ef0821874a5d65
Diane <yejimun.dev@gmail.com>
2023-09-20 16:04:57 +0900

    fix: 배경 블러 처리 추가 및 progressbar 디자인 변경

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -61,10 +61,13 @@ export const Questions = () => {
           {option.answer}
         </Button>
       ))}
-      <BottonSection>
-        🔮 <progress value={step} max={11} />
-        <div>Designed by Freepik</div>
-      </BottonSection>
+      <BottomSection>
+        <ProgressBar>
+          <div>🔮</div>
+          <progress value={step} max={11} />
+        </ProgressBar>
+        <div className="copyright">Designed by Freepik</div>
+      </BottomSection>
     </BlurBackground>
   );
 };
@@ -74,26 +77,40 @@ const BlurBackground = styled.div`
     width: 375px;
     height: 934px;
   }
-  position: absolute;
+  position: relative;
   width: 100%;
   height: 100%;
   flex-shrink: 0;
-  border-radius: 30px;
-  background: rgba(255, 255, 255, 0.3);
-  /* filter: blur(5px); */
   padding: 24px;
   display: flex;
   flex-direction: column;
   gap: 16px;
   align-items: center;
   justify-content: center;
+  z-index: 2;
+
+  &::after {
+    @media screen and (min-width: 1024px) {
+      width: 375px;
+      height: 934px;
+    }
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 30px;
+    background: rgba(255, 255, 255, 0.3);
+    filter: blur(5px);
+    z-index: -1;
+  }
 `;
 
 const Content = styled.div`
   color: #000;
   text-align: center;
   text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.4);
-  font-family: Heir of Light OTF;
   font-size: 20px;
   font-style: normal;
   font-weight: 700;
@@ -103,9 +120,35 @@ const Content = styled.div`
   white-space: break-spaces;
 `;
 
-const BottonSection = styled.div`
+const BottomSection = styled.div`
   position: absolute;
   bottom: 24px;
   color: #fff;
   text-align: center;
+
+  .copyright {
+    font-size: 10px;
+  }
+`;
+
+const ProgressBar = styled.div`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+  padding: 0px 8px;
+  height: 32px;
+  justify-items: center;
+  font-size: 16px;
+
+  progress {
+    appearance: none;
+    &::-webkit-progress-bar {
+      background: rgba(249, 240, 255, 0.7);
+      border-radius: 50px;
+    }
+    &::-webkit-progress-value {
+      background: rgba(131, 85, 224, 0.7);
+      border-radius: 50px;
+    }
+  }
 `;
