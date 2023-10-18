9410dd9740105601aa6aece21c196757d2584742
Diane <yejimun.dev@gmail.com>
2023-10-18 17:34:13 +0900

    fix: 결과 버그 수정

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { styled } from 'styled-components';
 import { stepState, answerState } from '@/recoil/state';
@@ -20,16 +20,23 @@ export const Questions = () => {
   const evalButtons: Array<ButtonColor> = ['aiYellow', 'aquaBlue'];
 
   const handleButtonClick = (option: AnswerType, step: number) => {
+    const currentStep = step + 1;
     const updateAnswers: MBTI[] =
-      answers.length < step + 1
+      answers.length < currentStep
         ? ([...answers, option.type] as MBTI[])
         : ([...answers.slice(0, step), option.type] as MBTI[]);
     setAnswers(updateAnswers);
-    if (updateAnswers.length === QnA.length) {
-      return (window.location.href = `${process.env.PUBLIC_URL}/results/${result}`);
+    if (currentStep === QnA.length) {
+      return;
     }
-    return setStep(step + 1);
+    return setStep(currentStep);
   };
+
+  useEffect(() => {
+    if (answers.length === QnA.length) {
+      window.location.href = `${process.env.PUBLIC_URL}/results/${result}`;
+    }
+  }, [answers, result]);
 
   return (
     <BlurBackground>
